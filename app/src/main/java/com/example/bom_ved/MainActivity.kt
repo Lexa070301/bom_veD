0a5013646fcd46edc987ad4f8d9308faf0e22396
nokk3r <nokk3r@mail.ru>
2021-10-21 20:57:33 +0300

    Added searchView.

--- app/src/main/java/com/example/bom_ved/MainActivity.kt
+++ app/src/main/java/com/example/bom_ved/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.example.bom_ved
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bom_ved.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
-import android.R
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 
-import android.app.SearchManager
-import android.widget.SearchView
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     private lateinit var bindingMain: ActivityMainBinding
 
 
@@ -23,16 +25,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         bindingMain = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(bindingMain.root)
         setSupportActionBar(bindingMain.appBar)
-        title = "My new title"
+        toolBarConfiguration(bindingMain.appBar)
         setupRecycleView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+
+        val search = menu?.findItem(R.id.searchView)
+        val searchView = search?.actionView as? SearchView
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
+
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun toolBarConfiguration(toolBar: androidx.appcompat.widget.Toolbar){
+        toolBar
     }
 
     private fun setupRecycleView(){
         bindingMain.recycleView.layoutManager = verticalLinearLayoutManager
         val itemDecorator = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+
+        val searchView = androidx.appcompat.widget.SearchView.generateViewId()
         bindingMain.recycleView.adapter = Adapter(UserHolder.createCollectionPictures(), this::showSnackbar)
     }
 
@@ -42,6 +64,18 @@ class MainActivity : AppCompatActivity() {
             "itemInfo" -> text = "Нажата карточка: " + picture.Name
             "like" -> text = "Нажат лайк: " + picture.Name
         }
+        Log.d("llolol", bindingMain.appBar.menu.toString())
         Snackbar.make(bindingMain.root, text, 3000).show()
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        if (newText != null) {
+            Log.d("thanks", newText)
+        }
+        return true
     }
 }
